a79c0d9e2829f4dd9639bca91be2fd4135396eb2
mj111x <cjulie4463@gmail.com>
2024-12-02 08:25:22 +0000

    작성된 파일 모두 저장

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function fetchTime() {
     // polling 방식으로 Flask 서버에서 시간 데이터를 요청
-    fetch('http://<your_server_ip>:5000/poll')
+    fetch('http://<your_server_ip>:8081/poll')
         .then(response => response.json())
         .then(data => {
             document.getElementById('time').innerText = '현재 시간: ' + data.time;
